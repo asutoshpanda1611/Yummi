ef7140b537fe05c1566ce621c0c13d11471ce45c
Asutosh panda <asutoshp97@gmail.com>
2024-09-17 16:25:06 +0530

    logo designed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,8 +9,6 @@ const Header = () => {
     <div className='header'>
         <div className="header-contents">
             <h2>Order your favourite food here</h2>
-            <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. our mission is to satisfy  your cravings and elevate your dining experience, one delicious meal at a time.</p>
-
             <a href='#explore-menu' onClick={() => setMenu("menu")} className={menu == 'menu' ? 'active' : ''} ><button>View Menu</button></a>
         </div>
     </div>
